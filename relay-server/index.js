da06f1b4bd1b206b12e84cc90af28bdff1742450
Lucas Lima <lucas.limalfss@gmail.com>
2022-12-31 21:27:05 -0300

    null

--- relay-server/index.js
+++ relay-server/index.js
@@ -2,8 +2,8 @@ import { RealtimeRelay } from './lib/relay.js';
 import dotenv from 'dotenv';
 dotenv.config({ override: true });
 
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-
+//const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const OPENAI_API_KEY = 'sk-QEBLZVTuFVxFVc8S73brRHYZx2HupCriIxBIG32741T3BlbkFJ1pUDrOKo-zD2oZEeNkmVIqv-mryYb4xpOYElzvwV4A'
 if (!OPENAI_API_KEY) {
   console.error(
     `Environment variable "OPENAI_API_KEY" is required.\n` +
--- src/pages/ConsolePage.tsx
+++ src/pages/ConsolePage.tsx
@@ -8,8 +8,8 @@
  * This will also require you to set OPENAI_API_KEY= in a `.env` file
  * You can run it with `npm run relay`, in parallel with `npm start`
  */
-const LOCAL_RELAY_SERVER_URL: string =
-  process.env.REACT_APP_LOCAL_RELAY_SERVER_URL || '';
+const LOCAL_RELAY_SERVER_URL: string = 'http://localhost:8081' ;
+  //process.env.REACT_APP_LOCAL_RELAY_SERVER_URL || '';
 
 import { useEffect, useRef, useCallback, useState } from 'react';
 
